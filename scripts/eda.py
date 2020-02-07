b50e07fb74d0a33bb393e2152cf3fb85d0d83805
Diego A. Diaz <diegodiaz@uchicago.edu>
2020-02-07 09:12:11 +0000

    Add linear regression model training script using economic data from public database. Split dataset, train the model, and evaluate performance. Print training and test R^2 scores.

--- scripts/eda.py
+++ scripts/eda.py
@@ -761,3 +761,33 @@ for i in range(len(future_years)):
 
 plt.plot(future_years, predicted_inflation_rates, color='red', linestyle='--')
 plt.show()
+# Change made on 2024-06-26 21:08:40.758949
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# Load dataset from a public database
+df = pd.read_csv('https://example_public_database.com/economic_data.csv')
+
+# Clean data
+df.dropna(inplace=True)
+
+# Select features and target variable
+X = df[['GDP', 'unemployment_rate']]
+y = df['inflation_rate']
+
+# Split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Evaluate the model
+train_score = model.score(X_train, y_train)
+test_score = model.score(X_test, y_test)
+
+# Print the results
+print(f"Training R^2 score: {train_score}")
+print(f"Test R^2 score: {test_score}")
