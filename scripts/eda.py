d688b90df3f0d9fd113cd3a2b8faa1f320ad4739
Diego A. Diaz <diegodiaz@uchicago.edu>
2020-02-22 00:51:20 +0000

    Add linear regression analysis code to analyze inflation rate based on GDP and unemployment rate data. Include data loading, cleaning, feature selection, model training, prediction, evaluation, and visualization.

--- scripts/eda.py
+++ scripts/eda.py
@@ -1202,3 +1202,40 @@ print("Mean Squared Error:", mse)
 coef_df = pd.DataFrame({'Variable': X.columns, 'Coefficient': model.coef_})
 print("Coefficients:")
 print(coef_df)
+# Change made on 2024-06-26 21:09:50.214819
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+# Load the dataset from a public database
+data = pd.read_csv('economics_data.csv')
+
+# Clean and preprocess the data
+data.dropna(inplace=True)
+
+# Select relevant features for the analysis
+X = data[['GDP', 'unemployment_rate']]
+y = data['inflation_rate']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+
+# Visualize the results
+plt.scatter(y_test, y_pred)
+plt.xlabel('Actual Inflation Rate')
+plt.ylabel('Predicted Inflation Rate')
+plt.title('Actual vs Predicted Inflation Rate')
+plt.show()
