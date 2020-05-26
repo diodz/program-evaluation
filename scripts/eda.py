0b96b1c49c1fe4a8329a4cb1627d5e3f834142f4
Diego A. Diaz <diegodiaz@uchicago.edu>
2020-05-26 21:14:04 +0000

    Add linear regression model implementation with data preprocessing, training, evaluation, and visualization.

--- scripts/eda.py
+++ scripts/eda.py
@@ -3609,3 +3609,38 @@ r_squared = model.score(X, y)
 
 # Print results
 print(f"R-squared value: {r_squared}")
+# Change made on 2024-06-26 21:16:35.033784
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+# Load the dataset from a public database
+data = pd.read_csv('https://example_public_database.com/data.csv')
+
+# Preprocess the data
+X = data.drop('target_variable', axis=1)
+y = data['target_variable']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Fit a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+
+# Visualize the results
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Values")
+plt.ylabel("Predicted Values")
+plt.title("Linear Regression Model Evaluation")
+plt.show()
