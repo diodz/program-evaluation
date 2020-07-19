a2b220acda7efd76be89da5589976e5dc6635b54
Diego A. Diaz <diegodiaz@uchicago.edu>
2020-07-19 22:33:23 +0000

    Add linear regression model code for inflation prediction using GDP and unemployment rate features with visualization.

--- scripts/eda.py
+++ scripts/eda.py
@@ -4994,3 +4994,42 @@ plt.xlabel('Actual Inflation')
 plt.ylabel('Predicted Inflation')
 plt.title('Actual vs Predicted Inflation')
 plt.show()
+# Change made on 2024-06-26 21:20:07.977508
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+# Load dataset from public database
+data = pd.read_csv('https://example.com/data.csv')
+
+# Explore the dataset
+print(data.head())
+
+# Preprocess the data
+X = data[['GDP', 'Unemployment Rate']]
+y = data['Inflation Rate']
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error: {mse}')
+
+# Visualize the results
+plt.scatter(X_test['GDP'], y_test, color='blue')
+plt.plot(X_test['GDP'], y_pred, color='red')
+plt.xlabel('GDP')
+plt.ylabel('Inflation Rate')
+plt.title('Linear Regression Model for Inflation Prediction')
+plt.show()
