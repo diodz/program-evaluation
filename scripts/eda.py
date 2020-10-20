1879051fc6e0954342ef5644bac0bc982a14c8a7
Diego A. Diaz <diegodiaz@uchicago.edu>
2020-10-20 03:29:11 +0000

    Add linear regression model training and evaluation code - Import necessary libraries - Load data from a public database - Data preprocessing: removing missing values and selecting features - Split data into training and testing sets - Build and train a linear regression model - Make predictions and evaluate the model - Print results and perform statistical analysis using statsmodels.

--- scripts/eda.py
+++ scripts/eda.py
@@ -6084,3 +6084,42 @@ y = data_cleaned['unemployment_rate']
 reg = LinearRegression().fit(X, y)
 print('Coefficients: ', reg.coef_)
 print('Intercept: ', reg.intercept_)
+# Change made on 2024-06-26 21:23:40.481470
+```python
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# Load data from a public database
+data = pd.read_csv('https://example.com/data.csv')
+
+# Data preprocessing
+data.dropna(inplace=True)
+X = data[['GDP', 'unemployment_rate', 'inflation_rate']]
+y = data['economic_growth']
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Build and train a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+predictions = model.predict(X_test)
+
+# Evaluate the model
+mse = np.mean((predictions - y_test) ** 2)
+r_squared = model.score(X_test, y_test)
+
+# Print results
+print(f'Mean Squared Error: {mse}')
+print(f'R-squared: {r_squared}')
+
+# Perform statistical analysis
+X_with_const = sm.add_constant(X_train)
+model_sm = sm.OLS(y_train, X_with_const).fit()
+print(model_sm.summary())
+```
