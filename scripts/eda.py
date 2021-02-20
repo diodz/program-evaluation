d758a78da13da09eedbd257ed4afe656cecdd0e2
Diego A. Diaz <diegodiaz@uchicago.edu>
2021-02-20 00:19:12 +0000

    feat: Added code to load data from a public database, preprocess, split into train and test sets, train a linear regression model, make predictions, evaluate the model using R-squared, and perform statistical analysis.

--- scripts/eda.py
+++ scripts/eda.py
@@ -7042,3 +7042,69 @@ model_sklearn = LinearRegression()
 model_sklearn.fit(X, y)
 print("Intercept:", model_sklearn.intercept_)
 print("Coefficients:", model_sklearn.coef_)
+# Change made on 2024-06-26 21:26:06.825827
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+
+# Load dataset from public database
+data = pd.read_csv('https://example.com/economic_data.csv')
+
+# Explore data
+print(data.head())
+
+# Perform data cleaning and preprocessing
+data = data.dropna()
+X = data[['GDP', 'unemployment_rate']]
+y = data['inflation_rate']
+
+# Fit a linear regression model
+model = LinearRegression()
+model.fit(X, y)
+
+# Get coefficients and intercept
+print("Coefficients:", model.coef_)
+print("Intercept:", model.intercept_)
+
+# Use statsmodels for detailed regression analysis
+X = sm.add_constant(X)
+model = sm.OLS(y, X).fit()
+print(model.summary())
+# Change made on 2024-06-26 21:26:11.368986
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+# Load the dataset from a public database
+data_url = 'https://url_to_public_database'
+df = pd.read_csv(data_url)
+
+# Preprocess the data
+df = df.dropna()
+X = df[['independent_var_1', 'independent_var_2']]
+y = df['dependent_variable']
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+r2 = r2_score(y_test, y_pred)
+
+# Perform statistical analysis
+X = sm.add_constant(X)
+model_stats = sm.OLS(y, X).fit()
+summary = model_stats.summary()
+
+print("R-squared:", r2)
+print(summary)
