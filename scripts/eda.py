904b7973931e51dca0c54149de464bbcbfb1e269
Diego A. Diaz <diegodiaz@uchicago.edu>
2022-11-28 01:00:36 +0000

    Added code for data loading, preprocessing, t-test comparison, linear regression modeling, mean squared error calculation, and data visualization. Conducted t-test to compare two groups and performed linear regression on dataset. Visualized the data with scatter plot for actual vs. predicted values.

--- scripts/eda.py
+++ scripts/eda.py
@@ -12301,3 +12301,44 @@ print(model.coef_)
 
 print("\nIntercept:")
 print(model.intercept_)
+# Change made on 2024-06-26 21:39:45.405833
+import pandas as pd
+import numpy as np
+from statsmodels.stats.weightstats import ttest_ind
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+# Load data from public database
+data = pd.read_csv('https://url_to_public_database/data.csv')
+
+# Data preprocessing
+# Add any necessary data cleaning steps here
+
+# Conduct t-test to compare two groups
+group1 = data[data['group'] == 'A']['value']
+group2 = data[data['group'] == 'B']['value']
+t_stat, p_value, _ = ttest_ind(group1, group2)
+print(f'T-test results: t-statistic={t_stat}, p-value={p_value}')
+
+# Perform linear regression
+X = data[['feature1', 'feature2']]
+y = data['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error: {mse}')
+
+# Visualize the data
+plt.scatter(data['feature1'], data['target'], color='blue', label='Actual')
+plt.scatter(data['feature1'], model.predict(data[['feature1', 'feature2']]), color='red', label='Predicted')
+plt.xlabel('Feature 1')
+plt.ylabel('Target')
+plt.legend()
+plt.show()
