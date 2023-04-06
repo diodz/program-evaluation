6d06f9c8cbf368df8d785df8ff5ed554e9f4c01a
Diego A. Diaz <diegodiaz@uchicago.edu>
2023-04-06 23:47:57 +0000

    Add linear regression model using sklearn and conduct OLS regression analysis with statsmodels.

--- scripts/eda.py
+++ scripts/eda.py
@@ -13292,3 +13292,39 @@ y_pred = model.predict(sm.add_constant(X_test))
 # Calculate the mean squared error
 mse = mean_squared_error(y_test, y_pred)
 print('Mean Squared Error:', mse)
+# Change made on 2024-06-26 21:42:28.744759
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+# Load data from a public database
+data = pd.read_csv("https://publicdata.com/economic_data.csv")
+
+# Data preprocessing
+data.dropna(inplace=True)
+X = data[['GDP', 'unemployment_rate']]
+y = data['inflation_rate']
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Fit a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+r2 = r2_score(y_test, y_pred)
+
+# Print the results
+print(f"R-squared: {r2}")
+
+# Conduct additional analysis such as OLS regression
+X_train = sm.add_constant(X_train)
+ols_model = sm.OLS(y_train, X_train).fit()
+print(ols_model.summary())
